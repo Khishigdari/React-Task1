b807bc0969cbf6cb06977cd9686ea6aabf40d9a8
Khishigdari <nbhishgee22@gmail.com>
2025-08-21 12:40:01 +0800

    todo

--- react-task1/src/components/button.jsx
+++ react-task1/src/components/button.jsx
@@ -2,13 +2,13 @@ export const Button = (props) => {
   return (
     <div>
       <div className="flex gap-[6px]">
-        <button className="py-1 px-3 bg-[#F3F4F6] hover:bg-[#3C82F6] rounded-[6px] text-[#363636] hover:text-white ml-4">
+        <button className="py-1 px-3 bg-[#F3F4F6] hover:bg-[#3C82F6] rounded-[6px] text-[#363636] hover:text-white ml-4 cursor-pointer">
           {props.all}
         </button>
-        <button className="py-1 px-3 bg-[#F3F4F6] hover:bg-[#3C82F6] rounded-[6px] text-[#363636] hover:text-white">
+        <button className="py-1 px-3 bg-[#F3F4F6] hover:bg-[#3C82F6] rounded-[6px] text-[#363636] hover:text-white cursor-pointer">
           {props.active}
         </button>
-        <button className="py-1 px-3 bg-[#F3F4F6] hover:bg-[#3C82F6] rounded-[6px] text-[#363636] hover:text-white">
+        <button className="py-1 px-3 bg-[#F3F4F6] hover:bg-[#3C82F6] rounded-[6px] text-[#363636] hover:text-white cursor-pointer">
           {props.completed}
         </button>
       </div>
